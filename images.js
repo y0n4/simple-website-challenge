5ec4f8ccfe8bb6508f449429a48e9b40a04da51f
yona <04l.yona@gmail.com>
2018-04-17 14:14:28 -0400

    some sample of gif images to get something to work on for now and get the webpage to work, will add more images to this file later on

---
+++ images.js
@@ -0,0 +1,13 @@
+const imageUrl = [
+  'http://78.media.tumblr.com/0f46eb9666a19ce388b9c82af86468f4/tumblr_oehshxMSNE1rnhl8xo1_540.gif',
+  'https://i.gifer.com/3sN2.gif',
+  'https://i.pinimg.com/originals/b9/b8/61/b9b8618f1eba0d816c8d66eac910b96c.gif',
+  'https://data.whicdn.com/images/304328264/original.gif',
+  'https://78.media.tumblr.com/f70791e9124bdd860ced97460618bb25/tumblr_p3qrktrSvp1x3ihv3o1_500.gif',
+  'https://media.giphy.com/media/k8Jw9IFAhSLO8/giphy.gif',
+  'https://i.kinja-img.com/gawker-media/image/upload/t_original/1363377053702569539.gif',
+  'http://68.media.tumblr.com/985399913f88afbb7373f4f7979a0881/tumblr_nx0gczUv2Z1qknyvjo1_500.gif',
+  'http://33.media.tumblr.com/a5100084ee818ad87524f0e7f84d73d2/tumblr_n9wv5uRaw61r9hdr9o1_500.gif',
+  'https://i.pinimg.com/originals/24/08/a4/2408a4b35748161f67478b246fed1b80.gif',
+  'https://i.imgur.com/fYcR8Ri.gif'
+]
