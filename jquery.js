98605ed7bf9f29cd86d564b8fd32c2154b2d71a7
yona <04l.yona@gmail.com>
2018-04-18 15:00:41 -0400

    implement mousemove event handle, window can now move from left to right, slide only within the window frame, not outside it's at it's fixed state and can only move from left to right

--- jquery.js
+++ jquery.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
   //everytime page refresh, new image pops up
   $("#window").css('background-image', 'url(' + randomImage() + ')');
+  //$("#window").css('background-image', 'url(https://i.pinimg.com/originals/aa/4a/ff/aa4aff547cf115081b52796ecda35331.gif)');
 
   //everytime button pressed, new image pops up
   $('.btn').click(function() {
+      console.log(randomImage());
     $("#window").css('background-image', 'url(' + randomImage() + ')');
+  });
+
+  //top panel
+  $(".panel").mousemove(function(e) {
+    var x = e.pageX - this.offsetLeft;
+    if(x <= 400) {
+      $("#top").css({'left': x});
+    }
   });
 });
