e6af2cd53b409181f1e9f76668a716d81e6f8930
yona <04l.yona@gmail.com>
2018-04-19 14:59:50 -0400

    mouse event for sound increase and decrease adjustments action occurs when the window panel is sliding left and right, user can now have the full experience of a virtual window

--- jquery.js
+++ jquery.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+  var $sound = $('audio');
+  //default volume should be silent
+  $sound.prop("volume", 0.0);
+
+  //volume function
+  window.setVolume = function(audio, vol) {
+      sounds[sound].volume = vol;
+  }
+
   //everytime page refresh, new image pops up
   $("#window").css('background-image', 'url(' + randomImage() + ')');
-  //$("#window").css('background-image', 'url(https://i.pinimg.com/originals/aa/4a/ff/aa4aff547cf115081b52796ecda35331.gif)');
+  //$("#window").css('background-image', 'url(https://i.giphy.com/media/J80drcoKo194I/giphy.webp)');
 
   //everytime button pressed, new image pops up
   $('.btn').click(function() {
@@ -9,11 +18,16 @@ $(document).ready(function(){
     $("#window").css('background-image', 'url(' + randomImage() + ')');
   });
 
-  //top panel
-  $(".panel").mousemove(function(e) {
+  //window slider
+  $("#window").mousemove(function(e) {
     var x = e.pageX - this.offsetLeft;
-    if(x <= 400) {
+    if(x <= 320) {
+      //panel moves left and right only
       $("#top").css({'left': x});
+      //sound gets louder when panel slide to left (open)
+      //sound gets lower when panel slide to right (close)
+      $sound.prop("volume", (1 - (304 / 2 - x) / (304 / 2)));
+      console.log($sound.prop);
     }
   });
 });
